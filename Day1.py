3c632c0ab6075375be575325c217a5e2efd37d4f
Drank0 <73241294+Dr4nk0@users.noreply.github.com>
2022-12-09 18:57:51 +0100

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,20 @@
+from heapq import nlargest
+Elfs_inventory = {}
+
+with open(r"C:\Users\ng92FFF\Documents\AventOfCode\Day1\input.txt", "r") as input_file :
+    data = input_file.read()
+    # First part of the challenge
+    groups = [ grp.split("\n") for grp in data.split("\n\n") ]
+    elf_number = 1
+    for elf in groups :
+        elf = list(map(int, elf))
+        elf_power = sum(elf)
+        Elfs_inventory[elf_number] = elf_power
+        elf_number = elf_number + 1
+    print("The biggest power holded by an Elf is", max(Elfs_inventory.values()))
+    # Second Part of the challenge
+    top_three_calories = nlargest(3, Elfs_inventory, key = Elfs_inventory.get)
+    calories_count = 0
+    for val in top_three_calories :
+        calories_count = calories_count + Elfs_inventory.get(val)
+    print ("The top three Elves are carrying a total of {0} calories".format(calories_count))
