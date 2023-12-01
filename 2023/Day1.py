25b2d7353bf3aa67ee964a9fdcf22c910b8e1cd5
Max <max@MBP.lan>
2023-12-01 11:45:16 +0100

    day1

---
+++ 2023/Day1.py
@@ -0,0 +1,28 @@
+with open('day1-input.txt') as f:
+    lines = f.readlines()
+
+#---------------------------------------------------------
+
+# digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+# total = 0
+
+# for line in lines : 
+    
+#     line_count = []
+#     line_value = 0
+
+#     for letter in line : 
+
+#         if letter in digits : line_count.append(letter)
+
+#     line_value = line_count[0] + line_count[-1]
+#     total = total + int (line_value)
+
+# print (total)
+
+#---------------------------------------------------------
+
+digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+alpha_digits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+
